217060308899f5c32817317c4b8aa44ccd02e1fa
Abhinav Chawda <123491911+Abhinavchawda@users.noreply.github.com>
2024-06-10 20:17:11 +0530

    Update Speaker_img.jsx

--- src/components/Speaker_img.jsx
+++ src/components/Speaker_img.jsx
@@ -6,7 +6,7 @@ const Speaker_img = (props) => {
             <div className='image-card rounded-2xl mb-16 md:my-0 min-h-[355px] w-[300px] shadow-xl hover:shadow-slate-400 text-pretty bg-[rgb(255,255,255)] scale-[98.5%] hover:scale-100'>
                 <div className='h-[250px] rounded-2xl shadow-md m-2 overflow-hidden'><img src={props.image} height={100} width={props.img_width}></img></div>
                 <div className='text-xl bg-slate-800 text-center font-serif font-semibold text-orange-500 flex justify-center items-center py-1 px-2'>{props.name}</div>
-                <div className='text-center text-gray-800 flex justify-center items-center px-3 py-1 font-serif'>{props.post}<br></br>
+                <div className='text-center text-gray-800 flex flex-col justify-center items-center px-1 py-2 font-serif'>{props.post}<br></br>
                     {props.post2}<br></br>
                     {props.post3}
                 </div>
